b8e3292857ee9f57b7e86e34eb44dc638e10f4ba
hsadhvani <hsadhvani@gmail.com>
2015-08-08 22:38:01 -0400

    disabled enter in function parameters

--- public/js/jquery_fns.js
+++ public/js/jquery_fns.js
@@ -2,4 +2,8 @@ var nextslide = function(to, from) {
 	$(from).fadeOut('fast', function() {
 		$(from).replaceWith($(to)).fadeIn('slow');
 	});
-};	
+};
+
+$(document).on('keypress', '.content_editable_div', function(e){
+	return e.which != 13;
+});
